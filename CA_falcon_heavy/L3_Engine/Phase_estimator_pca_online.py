129ffe338f8b5c62dc1d0fed64a02c8aaef6455f
Francesco De Lellis <francesco.delellis.93@gmail.com>
2024-07-26 19:28:20 +0200

    Updated CA integration

---
+++ CA_falcon_heavy/L3_Engine/Phase_estimator_pca_online.py
@@ -0,0 +1,138 @@
+import math
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+class Phase_estimator_pca_online:
+    def __init__(self, window_pca, interval_between_pca):
+        self.trajectory    = [] 
+        self.time_instants = []    
+
+        self.pca                  = PCA(n_components=1)
+        self.pca_direction        = 1                     # 1 or -1; used to preserve existing direction of the pca when recomputed
+        self.window_pca           = window_pca            # [s]
+        self.interval_between_pca = interval_between_pca  # [s]
+
+        self.pos_princ_comp        = 0  
+        self.pos_princ_comp_prev   = 0 
+        self.vel_princ_comp        = 0  
+        self.vel_princ_comp_prev   = 0 
+        self.acc_princ_comp        = 0    
+        self.pos_princ_comp_offset = 0
+
+        self.phase = 0                
+        self.is_first_estimation   = True  
+        self.time_last_pca         = None  
+        self.is_first_pca_computed = False
+        
+        self.amplitude_vel_p = 1
+        self.amplitude_vel_n = 1
+        self.amplitude_pos_n = 1
+        self.amplitude_pos_p = 1
+        
+        
+    def estimate_phase(self, position, current_time):
+        self.time_instants.append(current_time)
+        current_time = self.time_instants[-1]
+        self.trajectory.append(position)
+
+        if self.is_first_estimation: 
+            self.time_last_pca = current_time
+            self.is_first_estimation = False
+        
+        if not self.is_first_pca_computed:
+            if current_time - self.time_last_pca >= self.window_pca:
+                self.time_last_pca = current_time
+                self.compute_PCA()
+                self.is_first_pca_computed = True
+        else:
+            if current_time - self.time_last_pca >= self.interval_between_pca:
+                self.time_last_pca = current_time
+                self.compute_PCA()
+        
+        if self.is_first_pca_computed:  self.update_phase()
+        
+        return self.phase
+
+
+    def compute_PCA(self):
+        idx = 1
+        is_found = False
+        while not is_found:
+            if self.time_instants[-1] - self.time_instants[-idx] >= self.window_pca:
+                is_found = True
+            else:  idx += 1
+        
+        if self.is_first_pca_computed:  prev_pca_vec = self.pca.components_[0]
+
+        self.pca.fit(np.array(self.trajectory)[-idx:,:])
+
+        if self.is_first_pca_computed:
+            pca_vec = self.pca.components_[0]
+            if np.dot(pca_vec, prev_pca_vec) < 0:  self.pca_direction = -1 * self.pca_direction
+        
+        score = self.pca_direction * np.reshape(self.pca.transform(np.array(self.trajectory)[-idx:,:]),-1)  
+        max_score = max(score)
+        min_score = min(score)
+        self.pos_princ_comp_offset = (max_score + min_score)/2
+
+        array_1d = np.array(np.array(self.trajectory)[-4,:])        
+        array_2d = array_1d.reshape(1, -1)  # Reshape to (1, 3)
+        score = self.pca_direction * self.pca.transform(array_2d)
+        self.pos_princ_comp = score[0, 0] - self.pos_princ_comp_offset
+
+        array_1d = np.array(np.array(self.trajectory)[-5,:])        
+        array_2d = array_1d.reshape(1, -1)  # Reshape to (1, 3)
+        score = self.pca_direction * self.pca.transform(array_2d)
+        self.pos_princ_comp_prev = score[0, 0] - self.pos_princ_comp_offset
+        
+        sampling_time = self.time_instants[-5] - self.time_instants[-4]
+        self.vel_princ_comp_prev = (self.pos_princ_comp-self.pos_princ_comp_prev) / sampling_time
+
+        array_1d = np.array(np.array(self.trajectory)[-2,:])        
+        array_2d = array_1d.reshape(1, -1)  # Reshape to (1, 3)
+        score = self.pca_direction * self.pca.transform(array_2d)
+        self.pos_princ_comp = score[0, 0] - self.pos_princ_comp_offset
+
+        array_1d = np.array(np.array(self.trajectory)[-3,:])        
+        array_2d = array_1d.reshape(1, -1)  # Reshape to (1, 3)
+        score = self.pca_direction * self.pca.transform(array_2d)
+        self.pos_princ_comp_prev = score[0, 0] - self.pos_princ_comp_offset
+
+        sampling_time = self.time_instants[-2] - self.time_instants[-3]
+        self.vel_princ_comp = (self.pos_princ_comp-self.pos_princ_comp_prev) / sampling_time
+
+    def update_phase(self):   
+        array_1d = np.array(np.array(self.trajectory)[-1,:])        
+        array_2d = array_1d.reshape(1, -1)  # Reshape to (1, 3)
+        score = self.pca_direction * self.pca.transform(array_2d)
+        
+        self.pos_princ_comp_prev = self.pos_princ_comp
+        self.vel_princ_comp_prev = self.vel_princ_comp
+        
+        sampling_time = self.time_instants[-1] - self.time_instants[-2]
+
+        self.pos_princ_comp = score[0, 0] - self.pos_princ_comp_offset
+        self.vel_princ_comp = (self.pos_princ_comp-self.pos_princ_comp_prev) / sampling_time
+        self.acc_princ_comp    = (self.vel_princ_comp-self.vel_princ_comp_prev) / sampling_time
+
+        if self.pos_princ_comp_prev < 0 and self.pos_princ_comp >= 0 and self.vel_princ_comp > 0:
+            self.amplitude_vel_p = abs(self.vel_princ_comp)
+        if self.pos_princ_comp_prev >= 0 and self.pos_princ_comp < 0 and self.vel_princ_comp < 0:
+            self.amplitude_vel_n = abs(self.vel_princ_comp)
+        if self.vel_princ_comp_prev >= 0 and self.vel_princ_comp < 0 and self.acc_princ_comp < 0:
+            self.amplitude_pos_p = abs(self.pos_princ_comp)
+        if self.vel_princ_comp_prev < 0 and self.vel_princ_comp >= 0 and self.acc_princ_comp > 0:
+            self.amplitude_pos_n = abs(self.pos_princ_comp)
+        if self.pos_princ_comp >= 0:
+            position_normalized = self.pos_princ_comp / self.amplitude_pos_p
+        else:
+            position_normalized = self.pos_princ_comp / self.amplitude_pos_n
+        if self.vel_princ_comp >= 0:
+            velocity_normalized = self.vel_princ_comp / self.amplitude_vel_p
+        else:
+            velocity_normalized = self.vel_princ_comp / self.amplitude_vel_n
+
+        self.phase = math.atan2(-velocity_normalized, position_normalized)
+        
+        self.phase = np.mod(self.phase, 2*np.pi)  # wrap to [0, 2pi)
---
+++ CA_pop_synchronization/L3_server.py
@@ -0,0 +1,140 @@
+import socket
+import select
+import re
+import numpy as np
+import sys
+import signal
+from icecream import ic
+from L3_engine import L3Agent, Phase_estimator_pca_online
+
+ic.configureOutput(prefix='DEBUG | ')
+# ic.disable()                              # Uncomment to get useful debugging messages
+
+class L3_Wrapper():
+
+    def __init__(self, model_path, ID = 0, amp = 10, omega = 2, parts = 3, omega_parts = np.array([0, 3.4, 4.6]), c_strenght = 1.25):
+        self.ID = ID                # Python CA instance ID
+        self.amplitude = amp        # Movement amplitude
+        self.omega = omega          # Movement frequency                
+        self.x = 0 
+        self.y = 0  
+        self.z = 0  
+        self.z_amp_ratio = 0.1    
+        self.intial_position = 0
+        self.intial_phase = 0
+
+        self.l3_phase = []
+        self.l3_agent = L3Agent(parts, omega_parts, c_strenght, model_path)
+
+        self.window_pca           = 4     # duration of the time window [seconds] in which the PCA is operated
+        self.interval_between_pca = 1     # time interval [seconds] separating consecutive computations of the PCA
+
+        # self.parts_phases = []
+        self.estimators_live = []
+        for _ in range(parts):
+            self.estimators_live.append(Phase_estimator_pca_online(self.window_pca, self.interval_between_pca))
+
+        self.kuramoto_phases = [np.zeros(parts)]
+
+    # This function extracts the 3D data position coming from UE
+    def parse_TCP_string(self, string):
+        ic(string)
+        numbers = np.array([float(num) for num in re.findall(r'-?\d+\.?\d*', string)])
+        flag = len(numbers) == 3*self.l3_agent.n_nodes + 1
+        return flag, numbers[0:3], numbers[-1]
+    
+    def set_intial_position(self, position):
+        self.intial_position = position
+        self.y = -self.amplitude
+        self.z = 0
+        self.intial_phase = 0
+
+    # Calculates the next position and formats the message to be sent to UE for animation
+    def update_position(self, positions, delta_t, time):
+        # positions contains the neighbors 3D end effectors
+        theta = np.arctan2(self.z, self.y)
+        ic(theta)
+        self.l3_phase.append(theta)
+
+        ic(time)
+
+        phases = [theta - self.intial_phase]
+        for i in range(self.l3_agent.n_nodes - 1):
+            phases.append(agent.estimators_live[i].estimate_phase(positions[:, i+1], time) - np.pi) 
+
+        ic(phases)
+
+        self.l3_agent.dt = delta_t
+        # theta_next = self.l3_agent.l3_update_phase(np.array(phases))        
+        self.kuramoto_phases.append(self.l3_agent.update_phases(self.kuramoto_phases[-1]))                  # comment this and uncomment upper line if L3 is connected with VR agents 
+        theta_next = self.kuramoto_phases[-1][self.l3_agent.virtual_agent]
+
+        ic(theta_next)
+
+        self.y = self.amplitude * np.cos(theta_next)
+        self.z = self.amplitude * np.sin(theta_next)
+
+        message = 'X=' + str(self.intial_position[0]) + ' Y=' + str(self.intial_position[1] + self.y) + ' Z=' + str(self.intial_position[2] + self.z_amp_ratio * np.abs(agent.z))    # Format data as UE Vector
+        return message
+
+
+if __name__ == "__main__":
+    agent = L3_Wrapper('model')
+
+    # Set the server address and port (must match with socket in UE)
+    SERVER_ADDRESS = 'localhost'
+    SERVER_PORT = 12345
+
+    # Create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Bind the socket to the server address and port
+        server_socket.bind((SERVER_ADDRESS, SERVER_PORT))
+    except socket.error as e:
+        print("Connection error: %s" % e)
+
+    # Listen for incoming connections
+    server_socket.listen(1)         # Limit number of connections to L3 socket
+    print(f'Server listening on {SERVER_ADDRESS}:{SERVER_PORT}')
+
+    # Wait for a client connection
+    print('Waiting for a connection...')
+    connection, client_address = server_socket.accept()
+
+    # Register the signal handler for SIGINT (Control + C)
+    def signal_handler(sig, frame):
+        message = 'quit'
+        connection.send(message.encode('utf-8'))
+        print('Control + C pressed, closing socket...')
+        connection.close()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, agent.signal_handler)
+
+    print(f'Connection from {client_address}')
+
+    time = 0
+
+    # Receive data stream from UE
+    while True:    
+        try:
+            ic(f'waiting for data')
+            ready_to_read, _, _ = select.select([connection], [], [])
+            if ready_to_read: 
+                data = connection.recv(1024).decode()
+                _, position, delta_t = agent.parse_TCP_string(data)
+                position = np.tile(position, (3, 1)).T
+                ic(f'Received data: {data}')
+
+            if time == 0: agent.intial_position = position[:, 0]
+            
+            time += delta_t
+            ic(position, delta_t)
+            message = agent.update_position(position, delta_t, time)
+
+            _, ready_to_write, _ = select.select([], [connection], [])
+            if ready_to_write: connection.send(message.encode('utf-8'))
+            ic(f'Message sent: {message}')
+        except KeyboardInterrupt:
+            # Handle the Control + C key press gracefully
+            signal_handler(None, None)    
